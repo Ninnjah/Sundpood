942832999ef2c04004c37b4eecfd556026581aba
Ninnjah <45268461+Ninnjah@users.noreply.github.com>
2021-01-02 16:05:59 +0300

    Now sundpood loads dynamically with launcher

---
+++ data/keys.py
@@ -0,0 +1,42 @@
+
+forbidden = [
+    'Key.esc', 'Key.cmd', 'Key.cmd_r', 'Key.menu', 
+    'Key.pause', 'Key.scroll_lock', 'Key.print_screen', 
+    'Key.enter', 'Key.space', 'Key.backspace', 'Key.ctrl_l', 
+    'Key.ctrl_r', 'Key.alt_l', 'Key.alt_gr', 'Key.caps_lock',
+    'Key.num_lock', 'Key.tab', 'Key.shift', 'Key.shift_r',
+    'Key.insert', 'Key.delete', 'Key.home', 'Key.end', 
+    'Key.page_up', 'Key.page_down', '/' 
+]
+
+dict_ = {
+    ' ':' ',
+    'Key.f1' : 'F1',
+    'Key.f2' : 'F2',
+    'Key.f3' : 'F3',
+    'Key.f4' : 'F4',
+    'Key.f5' : 'F5',
+    'Key.f6' : 'F6',
+    'Key.f7' : 'F7',
+    'Key.f8' : 'F8',
+    'Key.f9' : 'F9',
+    'Key.f10' : 'F10',
+    'Key.f11' : 'F11',
+    'Key.f12' : 'F12',
+    '<96>' : 'Num 0',
+    '<97>' : 'Num 1',
+    '<98>' : 'Num 2',
+    '<99>' : 'Num 3',
+    '<100>' : 'Num 4',
+    '<101>' : 'Num 5',
+    '<102>' : 'Num 6',
+    '<103>' : 'Num 7',
+    '<104>' : 'Num 8',
+    '<105>' : 'Num 9',
+    '<110>' : 'Num .',
+    'Key.up' : 'Up',
+    'Key.down' : 'Down',
+    'Key.left' : 'Left',
+    'Key.right' : 'Right',
+    '\\\\' : '\\'
+}
